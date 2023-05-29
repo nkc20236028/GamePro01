35cae528f95b0e632f8cb32ab5ab0d65096e43f7
nkc20236028 <nkc.morishita.haruto@gmail.com>
2023-05-29 15:12:32 +0900

    0529

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -15,7 +15,7 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(-0.1f, 0, 0);
+        transform.Translate(-0.03f, 0, 0);
 
         if (transform.position.x < -8.0f)
         {
